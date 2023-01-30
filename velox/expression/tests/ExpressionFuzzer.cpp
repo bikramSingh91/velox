21d3470169267a06d2ec3db7175fd775e5eb1738
Bikramjeet Vig <bikramjeet@fb.com>
2023-01-30 14:08:00 -0800

    Add cast to the list of special forms in expression fuzzer
    This change adds cast as a special form which can be enabled via
    the special_form startup flag.
    
    Test Plan:
    Verified manually that fuzzer generates expression with cast

--- velox/expression/tests/ExpressionFuzzer.cpp
+++ velox/expression/tests/ExpressionFuzzer.cpp
@@ -70,8 +70,6 @@ DEFINE_bool(
     false,
     "Enable testing of function signatures with variadic arguments.");
 
-DEFINE_bool(enable_cast, false, "Enable testing with cast expression.");
-
 DEFINE_bool(
     choose_root_type_from_signature_template,
     false,
@@ -647,11 +645,15 @@ core::TypedExprPtr ExpressionFuzzer::generateExpression(
       chosenFunctionName = templateList[chosenExprIndex];
     }
 
-    expression = generateExpressionFromConcreteSignatures(
-        returnType, chosenFunctionName);
-    if (!expression && FLAGS_velox_fuzzer_enable_complex_types) {
-      expression = generateExpressionFromSignatureTemplate(
+    if (chosenFunctionName == "cast") {
+      expression = generateCastExpression(returnType);
+    } else {
+      expression = generateExpressionFromConcreteSignatures(
           returnType, chosenFunctionName);
+      if (!expression && FLAGS_velox_fuzzer_enable_complex_types) {
+        expression = generateExpressionFromSignatureTemplate(
+            returnType, chosenFunctionName);
+      }
     }
   }
   if (!expression) {
